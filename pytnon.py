50b46fcd14301ee71530ee3e23ab415bb71b2236
dradedzera <gustavodeandrade9090@gmail.com>
2024-04-23 16:39:10 -0300

    Criação variaveis.py

---
+++ pytnon.py
@@ -0,0 +1,12 @@
+nome="Gustavo Andrade"
+idade=16
+email="gustavodeandrade9090@gmail.com"
+telefone="190"
+nota=7.12
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
