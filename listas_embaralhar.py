125f96126bc4471b6058eae5969b275622efe86f
dradedzera <gustavodeandrade9090@gmail.com>
2024-05-23 09:34:29 -0300

    lista_embarelhar.py

--- listas_embaralhar.py
+++ listas_embaralhar.py
@@ -7,3 +7,6 @@ print(f"Lista Embaralhada: {alunos}")
 # Escolhe um aluno aleatoriamente
 aluno_sorteado = random.choice(alunos)
 print(f"Aluno sorteado: {aluno_sorteado}")
+# Ordena a lista Decrescente
+alunos.sort(reverse=True)
+print(f"Lista decrescente: {alunos}")
---
+++ listas_remover.py
@@ -0,0 +1,14 @@
+alunos=["Adriana", "Joao", "Breno", "Gustavo", "Hariel"]
+print(f"LIsta de alunos: {alunos}")
+# Método remove()
+alunos.remove("Adriana")
+print(f"Lista alunos: {alunos}")
+# Método pop(2)
+alunos.pop(2)
+print(f"Lista alunos: {alunos}")
+# Função del
+del alunos[3]
+print(f"Lista alunos: {alunos}")
+# Método clear()
+alunos.clear()
+print(f"Lista de alunos: {alunos}")
